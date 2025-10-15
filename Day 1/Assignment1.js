6e6e779b73d153eb1c46ec683975e1769e06117e
AndreaLincy <andrealincyj@gmail.com>
2025-10-15 15:13:07 +0530

    Add files via upload
    Day1

---
+++ Day 1/Assignment1.js
@@ -0,0 +1,11 @@
+var firstname = "Riya"
+var Companyname = "Testleaf"
+var mobilenumber = 12345678910
+var isAutomation = true
+var hasPlaywright
+
+console.log(firstname +" - " +typeof(firstname))
+console.log(Companyname +" - " + typeof(Companyname))
+console.log(mobilenumber + " - "+ typeof(mobilenumber))
+console.log(isAutomation +" - " + typeof(isAutomation))
+console.log(hasPlaywright +" - " + typeof(hasPlaywright))
